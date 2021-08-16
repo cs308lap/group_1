4e9f93d0fd3447679e3d2cb2995262966a785bed
Rishabh Garg <56191449+RishabhGarg108@users.noreply.github.com>
2021-08-16 14:32:59 +0530

    Added check for negative numbers

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,14 @@
 #include "functions.h"
+#include <stdexcept>
 
 int factorial(int n){
-    if(n!=1){
-	return(n * factorial(n-1));
+    if (n < 0)
+    {
+      throw std::runtime_error("Enter a positive value for n!");
+    }
+    if(n != 1)
+    {
+	    return(n * factorial(n-1));
     }
     else return 1;
 }
