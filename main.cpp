48a1cf8c2ba5caa51a12095307381e8c00b40aca
Rishabh Garg <56191449+RishabhGarg108@users.noreply.github.com>
2021-08-16 14:35:59 +0530

    Called factorial with a negative input

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(){
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    cout << "The factorial of -1 is " << factorial(-1) << endl;
     return 0;
 }
